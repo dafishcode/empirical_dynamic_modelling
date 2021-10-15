4efd62801119087d6cd5e77beffc5e354650f38c
Dominic Burrows <45342427+dmnburrows@users.noreply.github.com>
2021-10-15 15:24:17 -0700

    Add CCM sort function to preprocess CCM data

--- CCM.py
+++ CCM.py
@@ -1,3 +1,50 @@
+import admin_functions as adfn
+
+#=================================
+def CCM_sort(co, tr, dff):
+#=================================
+
+    """
+    This function sorts all trace and coord data into dictionary for CCM. 
+
+    Inputs:
+        co (np array): cells x XYZ coordinates and labels
+        trace (np array): cells x timepoints, raw fluorescence values
+        dff (np array): cells x timepoints, normalised fluorescence values
+        name (str): save name 
+    
+    Returns:
+        f_dict (dict): dictionary containing coordinates, mean trace, trace, mean dff, and dff for all neurons in brain. 
+    
+    """
+    import numpy as np
+    
+    
+    f_dict = {}
+
+    if co.shape[0] == tr.shape[0] and tr.shape[0] == dff.shape[0]:
+        sub_tr, sub_co = adfn.select_region(tr, co, 'all')
+        mean_tr =  np.apply_along_axis(np.mean, 0, sub_tr)
+
+        sub_dff, sub_co = adfn.select_region(dff, co, 'all')
+        mean_dff =  np.apply_along_axis(np.mean, 0, sub_dff)
+
+        f_dict = { 'coord': sub_co, 'mean trace' : mean_tr, 'trace': sub_tr, 'mean dff' : mean_dff, 'dff' : sub_dff}
+        return(f_dict)
+
+    else:
+        print("data wrong shape")
+        return()
+
+
+
+
+
+
+
+
+
+
 #===========================
 def E_ccm_heatmap(E, ccm, n_bins):
 #===========================
